5b778e2b793381a74a16a587f0510ff1cc93665c
llama-with-thumbs <vlad.filitovich@gmail.com>
2021-12-22 11:55:27 -0500

    fix(footer): link to covid-console branch

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -3,10 +3,10 @@ import gitHubLogo from '../../assets/icon/GitHub_Logo.png';
 import './footer.style.scss';
 
 export const footer = `<div class="footer__git footer-icon">
-<a href="https://github.com/llama-with-thumbs/covid-console#readme">
+<a href="https://github.com/llama-with-thumbs/covid-console/tree/covid-console">
   <img src="${octoCat}" alt="github-llama-with-thumbs" title="app's repo">
 </a>
-<a href="https://github.com/llama-with-thumbs/covid-console#readme">
+<a href="https://github.com/llama-with-thumbs/covid-console/tree/covid-console">
   <img src="${gitHubLogo}" alt="github-llama-with-thumbs" title="app's repo">
 </a>
 </div>`;
