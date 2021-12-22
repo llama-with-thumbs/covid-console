ba3bee2cb0c75472b3ec74ef922c7ff76b29dd07
llama-with-thumbs <vlad.filitovich@gmail.com>
2021-12-22 12:02:15 -0500

    fix(footer): focus on readme

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -3,10 +3,10 @@ import gitHubLogo from '../../assets/icon/GitHub_Logo.png';
 import './footer.style.scss';
 
 export const footer = `<div class="footer__git footer-icon">
-<a href="https://github.com/llama-with-thumbs/covid-console/tree/covid-console">
+<a href="https://github.com/llama-with-thumbs/covid-console/tree/covid-console#readme">
   <img src="${octoCat}" alt="github-llama-with-thumbs" title="app's repo">
 </a>
-<a href="https://github.com/llama-with-thumbs/covid-console/tree/covid-console">
+<a href="https://github.com/llama-with-thumbs/covid-console/tree/covid-console#readme">
   <img src="${gitHubLogo}" alt="github-llama-with-thumbs" title="app's repo">
 </a>
 </div>`;
